2927d84f6c07d166681f2dfa65f970e4b2d759ba
Don Button <donpublic@digitalgizmo.com>
2019-11-20 17:55:07 -0500

    initial commit

---
+++ treat_admin/journal/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Entry
+
+class EntryAdmin(admin.ModelAdmin):
+    # change_form_template = 'supporting/admin/narr_mblurb_change_form.html'
+    fieldsets = [
+        (None,  {'fields': ['title', 'slug', 'entry_date', 'ordinal', 
+            'lat', 'lon', 'zoom_level', 'is_flippable', 'entry_text']}),
+        # ('Behind the scenes',   {'fields': ['status_num', 'edited_by', 
+        #     'edit_date', 'notes']}),
+    ]
+    list_display = ('title', 'slug', 'entry_date')
+    # list_filter     = ['status_num'] 
+    # search_fields = ['title', 'slug']
+
+    # def truncated_map_blurb(self, obj):
+    #     return obj.map_blurb[:30] + "..."
+
+admin.site.register(Entry, EntryAdmin)
---
+++ treat_admin/journal/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+import datetime
+
+class Entry(models.Model):
+    slug = models.SlugField('short name', max_length=48, unique=True)
+    title = models.CharField(max_length=64)
+    entry_date = models.DateField()
+    ordinal = models.IntegerField('sequence in day', null=True, blank=True)
+    lat = models.FloatField('latitude')
+    lon = models.FloatField('longitude')
+    zoom_level = models.IntegerField(default=10)
+    is_flippable = models.BooleanField('Allow image flip', default=False)
+    entry_text = models.TextField(blank=True, default='')
+
+    class Meta:
+        ordering = ['entry_date', 'ordinal']
