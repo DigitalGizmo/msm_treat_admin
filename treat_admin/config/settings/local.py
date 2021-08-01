039808253dad23d00c6d5dea886ae88eb9aab464
Don Button <donpublic@digitalgizmo.com>
2021-08-01 13:33:21 -0400

    secret and allowed hosts for ocean

---
+++ treat_admin/config/settings/local.py
@@ -0,0 +1,12 @@
+"""
+Server Django settings.
+"""
+from .base import *
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Just in case I need different db settings
--- treat_admin/config/settings/staging.py
+++ treat_admin/config/settings/staging.py
@@ -3,6 +3,9 @@ Server Django settings.
 """
 from .base import *
 
+ALLOWED_HOSTS = ['msm-treat-admin.digitalgizmo.com', 'temp-treat-admin.digitalgizmo.com', '127.0.0.1']
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
