df67d7880b6a192a01cdcf9b9c5f6dba5c536d0f
Don Button <donpublic@digitalgizmo.com>
2021-08-01 15:11:30 -0400

    reinsert import os

--- treat_admin/config/settings/base.py
+++ treat_admin/config/settings/base.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
+import os
 
 # Normally you should not import ANYTHING from Django directly 
 # into your settings, but ImproperlyConfigured is an exception. 
